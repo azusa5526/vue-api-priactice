8da5ab0b5e2ac6aaf2dfc6d780d2e89a804ba0b1
azusa5526 <azusa5526@gmail.com>
2020-04-26 22:45:10 +0800

    4/26 中場休息結束

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -9,35 +9,27 @@
           <th width="130">購買時間</th>
           <th>Email</th>
           <th>購買物品</th>
-          <th width="80">數量</th>
           <th width="130">應付金額</th>
           <th width="130">是否付款</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="item in orderList" :key="item.id">
-          <td>{{item.category}}</td>
-          <td>{{item.title}}</td>
-          <td class="text-right">{{item.origin_price | currency}}</td>
-          <td class="text-right">{{item.price | currency}}</td>
+        <tr v-for="item in orders" :key="item.id">
+          <td>{{item.create_at}}</td>
+          <td>{{item.user.email}}</td>
+          <td>{{item.products.product_id}}</td>
+          <td class="text-right">{{item.total}}</td>
           <td>
-            <span class="text-success" v-if="item.is_enabled">啟用</span>
-            <span class="text-danger" v-else>未啟用</span>
-          </td>
-          <td>
-            <button class="btn btn-outline-primary btn-sm" @click="openModal(false, item)">編輯</button>
-            <button
-              class="btn btn btn-outline-danger btn-sm"
-              @click="openDeleteProductModal(item)"
-            >刪除</button>
+            <span class="text-success" v-if="item.is_paid">已付款</span>
+            <span class="text-danger" v-else>未付款</span>
           </td>
         </tr>
       </tbody>
     </table>
 
     <!-- BS pagination -->
-    <Pagination :pagination="pagination" @changePage="getOrderLsit"></Pagination>
+    <Pagination :pagination="pagination" @changePage="getOrders"></Pagination>
 
   </div>
 </template>
@@ -53,7 +45,7 @@ export default {
 
   data() {
     return {
-      orderList: [],
+      orders: [],
       tempProduct: {},
       isNew: false,
       isLoading: false,
@@ -65,7 +57,7 @@ export default {
   },
 
   methods: {
-    getOrderLsit(page = 1) {
+    getOrders(page = 1) {
       //default page 1
       //console.log(process.env.API_PATH, process.env.CUSTOM_PATH);
       const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/orders?page=:page`;
@@ -75,7 +67,7 @@ export default {
       this.$http.get(api).then(response => {
         console.log(response.data);
         vm.isLoading = false;
-        vm.orderList = response.data.orders;
+        vm.orders = response.data.orders;
         vm.pagination = response.data.pagination;
       });
     },
@@ -83,7 +75,7 @@ export default {
   },
 
   created() {
-    this.getOrderLsit();
+    this.getOrders();
   }
 };
 </script>
