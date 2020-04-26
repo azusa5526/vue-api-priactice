d9f67d8a36417b489fbf107c98757bd53b6ab251
azusa5526 <azusa5526@gmail.com>
2020-04-26 21:35:04 +0800

    Revert "Update Coupon.vue"
    This reverts commit cb75592f17615ad9dc529b478e9cbeec47b6a988.

--- src/components/Coupon.vue
+++ src/components/Coupon.vue
@@ -1,5 +1,13 @@
 <template>
-      <table class="table mt-4">
+  <div>
+    <!-- vue-loading-overlay -->
+    <loading :active.sync="isLoading"></loading>
+
+    <div class="text-right">
+      <button class="btn btn-primary mt-4" @click="openModal(true)">建立新的產品</button>
+    </div>
+
+    <table class="table mt-4">
       <thead>
         <tr>
           <th width="150">分類</th>
@@ -10,17 +18,6 @@
           <th width="130">編輯</th>
         </tr>
       </thead>
-
-  
-  
-  <div>
-    <!-- vue-loading-overlay -->
-    <loading :active.sync="isLoading"></loading>
-
-    <div class="text-right">
-      <button class="btn btn-primary mt-4" @click="openModal(true)">建立新的產品</button>
-    </div>
-
 
       <tbody>
         <tr v-for="item in products" :key="item.id">
